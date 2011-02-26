7d8f5a7ceb4b02d957673c6eaf6ddc225b08d0e7
Christina Isakova <christina.isakova@gmail.com>
2011-02-26 18:37:03 +0200

    added calc

---
+++ calc.rb
@@ -0,0 +1,25 @@
+def ask question
+	goodAnswer = false
+	while (not goodAnswer)
+		puts question
+		reply = gets.chomp.downcase
+
+		if (reply == 'да' or reply == 'нет')
+			goodAnswer = true
+			if (reply == 'да')
+				answer = true
+			else
+				answer = false
+			end
+		else
+			puts 'Пожалуйста, отвечайте "да" или "нет"'
+		end
+	end
+answer
+end
+
+ask 'Вам нравится такос?'
+ask 'Вам нравится буритос?'
+wets_bed = ask 'Вы мочитесь в постель?'
+ask 'Вам нравится тамалес?'
+puts wets_bed
